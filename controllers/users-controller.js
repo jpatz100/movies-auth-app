022058c3bf8df3139af210e1a143751793f4f0db
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-11-02 03:55:46 -0400

    change redirect after register new user to movies route

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -13,7 +13,7 @@ usersController.create = (req, res) => {
     }).then(user => {
         req.login(user, (err) => {
             if (err) return next(err);
-            res.redirect('/user');
+            res.redirect('/movies');
         });
     }).catch(err => {
         console.log(err);
